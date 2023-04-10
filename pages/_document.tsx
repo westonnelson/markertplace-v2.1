a1df42191c70d9556993a6a563c66f6f17676dcb
westonnelson <dogooddowell@protonmail.com>
2023-04-10 02:55:17 -0500

    1 file modified

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -77,6 +77,33 @@ class MyDocument extends Document {
             dangerouslySetInnerHTML={{ __html: getCssText() }}
           />
         </Head>
+
+        <meta charSet="utf-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+        {/* Meta tags */}
+        <meta name="keywords" content="nft, ethereum, protocol" />
+        <meta name="keywords" content="NFT, API, Protocol" />
+
+        {/* Favicon */}
+        <link
+          rel="shortcut icon"
+          type="image/svg"
+          href="https://nftearth.exchange/nftearth-icon-new.png"
+        />
+
+        {/* Reservoir meta tags */}
+        <meta property="reservoir:title" content="Reservoir Market" />
+        <meta property="reservoir:icon" content="/reservoir-source-icon.png" />
+        <meta
+          property="reservoir:token-url-mainnet"
+          content="/collection/ethereum/${contract}/${tokenId}"
+        />
+        <meta
+          property="reservoir:token-url-polygon"
+          content="/collection/polygon/${contract}/${tokenId}"
+        />
         <body>
           <Main />
           <NextScript />
